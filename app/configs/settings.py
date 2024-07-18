e879b8e3d98451ef0ebd1162e18629b13a83a57e
André Pereira <andresosexatas@gmail.com>
2024-07-18 02:07:45 -0300

    fix: endpoints

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -1,19 +1,21 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_path = os.path.join(os.path.dirname(__file__), '../.env')
+env_path = os.path.join(os.path.dirname(__file__), "../.env")
 load_dotenv(env_path)
+
 
 class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=env_path, env_file_encoding="utf-8")
     postgres_user: str
     postgres_password: str
     postgres_db: str
-    postgres_name: str
+    postgres_host: str
+    postgres_port: str
     weather_api_key: str
-    
+
     @property
     def postgres_url(self):
-        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_db}/{self.postgres_name}"
+        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, Integer, String, Float, DateTime, JSON
+from sqlalchemy import JSON, Column, DateTime, Float, Integer, String
+
 from app.database import Base
 
 
 class WeatherData(Base):
     __tablename__ = "weather_data"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     city_id = Column(Integer, index=True)
     temperature = Column(Float)
@@ -14,11 +15,12 @@ class WeatherData(Base):
     data = Column(JSON)
     timestamp = Column(DateTime, default=datetime.now(timezone.utc))
 
+
 class RequestLog(Base):
     __tablename__ = "request_log"
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    user_defined_id = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
     total_cities = Column(Integer)
     cities_collected = Column(Integer, default=0)
     timestamp = Column(DateTime, default=datetime.now(timezone.utc))
